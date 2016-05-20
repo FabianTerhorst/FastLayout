fd5f1257f6f046b84bcad88a0b54b27c89721766
fabianterhorst <fabian.terhorst@gmail.com>
2016-05-20 21:57:58 +0200

    improve sample

---
+++ app/src/main/java/io/fabianterhorst/fastlayout/sample/MyLayoutConverter.java
@@ -0,0 +1,25 @@
+package io.fabianterhorst.fastlayout.sample;
+
+import io.fabianterhorst.fastlayout.converters.LayoutConverter;
+import io.fabianterhorst.fastlayout.converters.LayoutAttribute;
+import io.fabianterhorst.fastlayout.annotations.Converter;
+
+/**
+ * Created by fabianterhorst on 20.05.16.
+ */
+@Converter
+public class MyLayoutConverter extends LayoutConverter {
+
+    public MyLayoutConverter() {
+
+    }
+
+    @Override
+    public LayoutAttribute onConvertLayoutAttribute(Object attributeValue, String attributeName, boolean isString) {
+        /*switch (attributeName) {
+            case "android:layout_marginLeft":
+                return new LayoutAttribute(LayoutAttribute.Type.PARAM, setter("", "", false), attributeValue, false);
+        }*/
+        return null;
+    }
+}
