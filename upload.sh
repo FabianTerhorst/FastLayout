97c3fe24e578ae275bbbc309e4b79a7886d5bfc5
fabianterhorst <fabian.terhorst@gmail.com>
2016-05-23 12:09:32 +0200

    add upload script

---
+++ upload.sh
@@ -0,0 +1,5 @@
+./gradlew clean :fastlayout:bintrayUpload
+./gradlew clean :fastlayout-processor:bintrayUpload
+./gradlew clean :fastlayout-annotations:bintrayUpload
+./gradlew clean :fastlayout-gradle-plugin:bintrayUpload
+./gradlew clean :fastlayout-converters:bintrayUpload
