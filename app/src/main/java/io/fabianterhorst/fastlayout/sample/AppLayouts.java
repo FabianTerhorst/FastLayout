9334f2cbf125c242928f4700a47c59c481cc94a2
drivfe <ivanfelicianox@gmail.com>
2016-06-07 21:24:32 -0400

    TextView fixes

--- app/src/main/java/io/fabianterhorst/fastlayout/sample/AppLayouts.java
+++ app/src/main/java/io/fabianterhorst/fastlayout/sample/AppLayouts.java
@@ -5,8 +5,8 @@ import io.fabianterhorst.fastlayout.annotations.Layouts;
 /**
  * Created by fabianterhorst on 14.05.16.
  */
-//@Layouts(all = true/*, exclude = {R.layout.item_sample}*/)
-@Layouts(ids = {R.layout.activity_main, R.layout.item_sample, R.layout.card_view})
+@Layouts(all = true/*, exclude = {R.layout.item_sample}*/)
+//@Layouts(ids = {R.layout.activity_main, R.layout.item_sample, R.layout.card_view})
 //@Layouts(layouts = {"activity_main"})
 //@Layouts
 public class AppLayouts {
