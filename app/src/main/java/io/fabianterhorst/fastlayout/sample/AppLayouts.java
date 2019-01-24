8d33d829e769d42cf015e7c477c77ca7fda26f03
xie <xieshuhua@163.com>
2019-01-24 11:20:16 +0800

    Run in the current Android Studio Version

--- app/src/main/java/io/fabianterhorst/fastlayout/sample/AppLayouts.java
+++ app/src/main/java/io/fabianterhorst/fastlayout/sample/AppLayouts.java
@@ -7,9 +7,9 @@ import io.fabianterhorst.fastlayout.annotations.Layouts;
 /**
  * Created by fabianterhorst on 14.05.16.
  */
-@Layouts(all = true/*, exclude = {R.layout.item_sample}*/)
+//@Layouts(all = true, exclude = {R.layout.card_view})
 //@Layouts(ids = {R.layout.activity_main, R.layout.item_sample, R.layout.card_view})
-//@Layouts(layouts = {"activity_main"})
+@Layouts(layouts = {"item_sample"})
 //@Layouts
 public class AppLayouts {
 
